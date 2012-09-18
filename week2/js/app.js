7388df0c922703d56c5a1bfa30a5d74cad5d0aee
Ryan Guzal <guzalrya@msu.edu>
2012-09-17 22:41:04 -0400

    Week 2 update
    all wek two assignments

---
+++ week2/js/app.js
@@ -0,0 +1,15 @@
+function hello(){
+alert("Hello World!");
+}
+
+function browcheck(){
+alert("Browser Version: " + navigator.appVersion + "");
+
+document.getElementById("browver").innerHTML=txt;
+/*
+txt = "<p>Browser CodeName: " + navigator.appCodeName + "</p>";
+txt+= "<p>Browser Name: " + navigator.appName + "</p>";
+txt+= "<p>Browser Version: " + navigator.appVersion + "</p>";
+
+*/
+}
